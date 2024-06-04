446feb8ba934cb1095edd10d27a22dcd1ca6477f
reikstar <reikstar1@gmail.com>
2024-06-04 22:20:18 +0300

    SafePrime genration added

---
+++ Math/test_SafePrime.py
@@ -0,0 +1,21 @@
+from time import time
+from Prime import getSafePrime
+
+def measure_execution_time(func, *args, **kwargs):
+    start_time = time()
+    result = func(*args, **kwargs)
+    end_time = time()
+    execution_time = end_time - start_time
+    return result,execution_time
+
+iterations = 50
+bits = 1000
+total_getSafePrime = 0
+for i in range(iterations):
+    
+    total_getSafePrime += measure_execution_time(getSafePrime,bits)[1]
+
+
+print(total_getSafePrime/iterations)
+
+
