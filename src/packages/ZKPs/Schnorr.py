76e492aab7e6721535ed4c8df14471a04eab9c1a
reikstar <reikstar1@gmail.com>
2024-06-14 21:22:09 +0300

    schnorr's added

---
+++ src/packages/ZKPs/Schnorr.py
@@ -0,0 +1,56 @@
+from hashlib import sha512
+from secrets import randbelow
+from packages.math.mod_expo import base_k_exp
+from packages.AsymmetricCiphers.ElGamal import MulElGamal
+import gmpy2 as gmp
+
+K = 3 #base_k_exp base
+
+
+def generate_proof(p, q, generator, priv_key, pub_key, other_info = None):
+
+    h = sha512()
+    rnd_value = randbelow(q)
+    commitment = base_k_exp(generator, rnd_value, p, K)
+    
+    if other_info is not None:
+        str_val = str(other_info)
+    else:
+        str_val = ""
+
+    hash_input = str(p) + str(q) + str(generator) + str(pub_key) + str(commitment) + str_val
+    print(hash_input.encode())
+
+    h.update(hash_input.encode())
+    print(int(h.hexdigest(), 16))
+    challenge = gmp.f_mod(int(h.hexdigest(), 16), q)
+    print(challenge)
+    prod = gmp.f_mod(gmp.mul(challenge, priv_key), q)
+    response = gmp.f_mod(rnd_value + prod, q)
+
+    return(int(commitment), int(response))
+
+
+def verify_proof(p, q, generator, pub_key, commitment, response, other_info = None):
+
+    h = sha512()
+
+    if other_info is not None:
+        str_val = str(other_info)
+    else:
+        str_val = ""
+
+    hash_input = str(p) + str(q) + str(generator) + str(pub_key) + str(commitment) + str_val
+    print(hash_input.encode())
+    h.update(hash_input.encode())
+    print(int(h.hexdigest(), 16))
+    challenge = gmp.f_mod(int(h.hexdigest(), 16), q)
+    print(challenge)
+
+    if base_k_exp(generator, response, p, K) == gmp.f_mod(gmp.mul(commitment, base_k_exp(pub_key, challenge, p, K)), p):
+        return True
+    else:
+        return False
+
+
+
