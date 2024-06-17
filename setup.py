492f9b6e052d6c5b3de13eb9eb943147f6cfd759
reikstar <reikstar1@gmail.com>
2024-06-17 18:02:38 +0300

    designated verifier re-encryption proof

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='evoting',
-    version='1.0',
+    name="evoting",
+    version="1.0",
     packages=find_packages(),
 )
--- src/packages/ZKPs/VerifiableElGamal.py
+++ src/packages/ZKPs/VerifiableElGamal.py
@@ -1,4 +1,5 @@
 from secrets import randbelow
+from src.packages.AsymmetricCiphers.ElGamal import AddElGamal
 from src.packages.math.mod_expo import base_k_exp
 from src.packages.Utils.utils import fiat_shamir
 import gmpy2 as gmp
@@ -106,3 +107,131 @@ def decryption_verify(
         return False
 
     return True
+
+
+def re_encyption_proof(
+    p,
+    q,
+    generator,
+    verifier_pub_key,
+    pub_key,
+    r_enc_factor,
+    ciphertext,
+    r_enc_ciphertext,
+    other_info=None,
+):
+    if other_info is not None:
+        str_val = str(other_info)
+    else:
+        str_val = ""
+
+    rand_values = [randbelow(q) for i in range(3)]  # d,w,r
+    commitment = []
+    response = []
+
+    commitment.append(base_k_exp(generator, rand_values[0], p, K))  # a
+    commitment.append(base_k_exp(pub_key, rand_values[0], p, K))  # b
+    commitment.append(
+        gmp.f_mod(
+            gmp.mul(
+                base_k_exp(generator, rand_values[1], p, K),
+                base_k_exp(verifier_pub_key, rand_values[2], p, K),
+            ),
+            p,
+        )
+    )  # s
+
+    hash_value = fiat_shamir(
+        p,
+        q,
+        generator,
+        verifier_pub_key,
+        pub_key,
+        commitment,
+        ciphertext,
+        r_enc_ciphertext,
+        str_val,
+    )
+    challenge = gmp.f_mod(hash_value, q)
+
+    response.append(rand_values[1])  # w
+    response.append(rand_values[2])  # r
+    response.append(
+        gmp.f_mod(rand_values[0] + gmp.mul(r_enc_factor, challenge + rand_values[1]), q)
+    )  # t
+
+    return (commitment, response)
+
+
+def re_encyption_verify(
+    p,
+    q,
+    generator,
+    verifier_pub_key,
+    pub_key,
+    commitment,
+    response,
+    ciphertext,
+    r_enc_ciphertext,
+    other_info=None,
+):
+    if other_info is not None:
+        str_val = str(other_info)
+    else:
+        str_val = ""
+
+    hash_value = fiat_shamir(
+        p,
+        q,
+        generator,
+        verifier_pub_key,
+        pub_key,
+        commitment,
+        ciphertext,
+        r_enc_ciphertext,
+        str_val,
+    )
+    challenge = gmp.f_mod(hash_value, q)
+
+    inv_ciphertext = []
+    for i in range(2):
+        inv_ciphertext.append(gmp.invert(ciphertext[i], p))
+
+    left_side = commitment[2]
+
+    right_side = gmp.f_mod(
+        gmp.mul(
+            base_k_exp(generator, response[0], p, K),
+            base_k_exp(verifier_pub_key, response[1], p, K),
+        ),
+        p,
+    )
+    # First equality.
+    if left_side != right_side:
+        print("first eq failed")
+        return False
+
+    left_side = base_k_exp(generator, response[2], p, K)
+    frac = gmp.f_mod(gmp.mul(r_enc_ciphertext[0], inv_ciphertext[0]), p)
+    exponent = gmp.f_mod(challenge + response[0], q)
+
+    right_side = gmp.f_mod(gmp.mul(commitment[0], base_k_exp(frac, exponent, p, K)), p)
+
+    # Second equality.
+    if left_side != right_side:
+        print("2nd eq failed")
+        return False
+
+    left_side = base_k_exp(pub_key, response[2], p, K)
+    frac = gmp.f_mod(gmp.mul(r_enc_ciphertext[1], inv_ciphertext[1]), p)
+
+    right_side = gmp.f_mod(gmp.mul(commitment[1], base_k_exp(frac, exponent, p, K)), p)
+
+    # Third equality.
+    if left_side != right_side:
+        print("3rd eq failed")
+        return False
+
+    return True
+
+
